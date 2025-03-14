148d031000e2fe59dda843d6b852ff21a985f77c
Tanmay <tanmayshinde32@gmail.com>
2025-03-14 23:16:30 +0530

    adding files to github

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({ data }) => {
+    console.log(data);
+    return (
+        <div className='cardContainer'>
+            
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Newsapp.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Card from './Card'
+const Newsapp = () => {
+  const API_KEY="1b3886ba52d1472dac5314501faa50ea";
+  const [inputSearch,setInputSearch]=React.useState('india');
+  const [fetchedData,setFetchedData]=React.useState(null);
+  const getData=async()=>{
+    const response=await fetch(`https://newsapi.org/v2/everything?q=${inputSearch}&apiKey=${API_KEY}`);
+    const data=await response.json();
+    console.log(data.articles);
+    setFetchedData(data.articles);
+  }
+
+  const handleInput=(e)=>{
+    console.log(e.target.value);
+    setInputSearch(e.target.value);
+  }
+  return (
+    <div>
+      <nav>
+          <h1>Trendy News</h1>
+          <ul>
+            <a href="">All News</a>
+            <a href="">Trending</a>
+          </ul>
+
+          <div className="searchBar">
+            <input type="text" placeholder='Search News' onChange={handleInput}/>
+            <button  onClick={getData}>Search</button>
+          </div>
+      </nav>
+      <p className='head'>Stay Updated With Trendy News</p>
+      <div className="categoryBtn">
+        <button>Politics</button>
+        <button>Sports</button>
+        <button>Entainment</button>
+        <button>Health</button>
+      </div>
+
+      
+      <div>
+        <Card  data={fetchedData}/>
+      </div>
+    </div>
+  )
+}
+
+export default Newsapp
